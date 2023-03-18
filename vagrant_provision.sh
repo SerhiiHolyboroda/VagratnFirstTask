1c8d426359c3c1c4c67401f8e42984327d860e2a
SerhiiHolyboroda <holyboroda.serhii@gmail.com>
2023-03-18 22:03:19 +0200

    adding files

---
+++ vagrant_provision.sh
@@ -0,0 +1,7 @@
+sudo apt install wget
+# Скачую wget
+sudo apt-get install lamp-server^ -y
+# php, apache, mysql
+
+sudo wget -P /var/www/html -O installer.php https://github.com/mplesha/NoviNano/releases/download/v1.0/20180706_novinano_mr_301b6110dc5ef0dd6130180706071745_installer.php
+sudo wget -P /var/www/html -O archive.zip https://github.com/mplesha/NoviNano/releases/download/v1.0/20180706_novinano_mr_9d7561b2584268389437180706072703_archive.zip
